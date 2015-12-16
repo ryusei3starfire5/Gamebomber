a4d1f192383f6cb620346927b2506e5f8bb4f8b8
関口流星 <star_ryus@yahoo.co.jp>
2015-12-16 17:09:47 +0900

    first commit

---
+++ ResultViewContoller.m
@@ -0,0 +1,13 @@
+//
+//  ResultViewContoller.m
+//  escape
+//
+//  Created by 関口流星 on 2015/12/09.
+//  Copyright © 2015年 関口流星. All rights reserved.
+//
+
+#import "ResultViewContoller.h"
+
+@implementation ResultViewContoller
+
+@end
